fcab646a86a58baaf32d2f4789cfc8d9bad960ad
Trish <trishaannmonzon@gmail.com>
2025-07-01 14:20:17 +0800

    dashboard

---
+++ ongoing/src/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      <header >
+        <p>
+          Dashboard
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default Dashboard
