3369ff339eef5fc04d2da51a54f9e6597bf855a5
Trish <trishaannmonzon@gmail.com>
2025-07-02 17:29:19 +0800

    header2'

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from './Header'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+
+    <div>
+      <Header/>
+    </div>
   )
 }
 
--- src/Header.js
+++ src/Header.js
@@ -4,29 +4,39 @@ import { MdAccountCircle } from "react-icons/md";
 
 function Header() {
   return (
-    <div className='dashboard-bg'>
-      <img className='bg-image' src='images/plm_trans.png' alt='PLM'/>
-      <div className='title1'>
-          <img src="plm_logo.png" alt='PLM Logo' className='logo-header'/>
-          <h1 className='title'>
-            OFFICE OF THE UNIVERSITY REGISTRAR
-          </h1>
-          <p className='subTitle'>
-            Pamantasan ng Lungsod ng Maynila
-          </p>
-      </div>
-      <div className='emp'>
-        <div className='emp-text'>
-          <h1 className='emp-acc'>
-            Employee Name
-          </h1>
-          <p className='desc'>
-            Clerk-in-Charge
-          </p>
+    <div className='Header'>
+      <div className='dashboard-bg'>
+        <img className='bg-image' src='images/plm_trans.png' alt='PLM'/>
+        <div className='title1'>
+            <img src="plm_logo.png" alt='PLM Logo' className='logo-header'/>
+            <h1 className='title'>
+              OFFICE OF THE UNIVERSITY REGISTRAR
+            </h1>
+            <p className='subTitle'>
+              Pamantasan ng Lungsod ng Maynila
+            </p>
         </div>
-      <MdAccountCircle className='acc-icon'/>
-      </div>
+        <div className='emp'>
+          <div className='emp-text'>
+            <h1 className='emp-acc'>
+              Employee Name
+            </h1>
+            <p className='desc'>
+              Clerk-in-Charge
+            </p>
+          </div>
+          <MdAccountCircle className='acc-icon'/>
+        </div>
 
+      </div>
+      <div className='tabs'> 
+        <h2>
+          <a href=' '>Dashboard</a>
+        </h2>
+        <h2>
+          <a href=' '>Requests</a>
+        </h2>
+      </div>
     </div>
   )
 }
